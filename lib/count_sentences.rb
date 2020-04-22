3b821717b0791f0cc69b5fbec6ba487c4cbb40d3
David Munro <dbhmunro@gmail.com>
2020-04-22 21:03:33 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,8 +15,7 @@ class String
   end
 
   def count_sentences
-    self.to_a
-    self.each.split(". ")
+    self.split(". ")
     self.each.split("? ")
     self.each.split("! ")
     self.flatten.count
